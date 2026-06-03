7ec80661bc4d6448e41e1dc6dd5ecbbcfa27ef6d
Mohammad Alman Farooqui <almanfarooqui5@gmail.com>
2026-06-03 12:32:31 +0530

    readme project description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="tempora-cli",
-    version="2.0.0",
+    version="2.0.1",
     description="Tempora: Automated Log Integrity Monitor and Forensic Dashboard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Mohammad Alman Farooqui",
     packages=find_packages(),
     install_requires=[
